edcb93db14383e538bf052d8d600b8620bc262d5
Petter Berntsen <petterb@gmail.com>
2009-09-13 18:08:54 +0200

    commit -a

--- gen/no/petter/R.java
+++ gen/no/petter/R.java
@@ -11,11 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int android=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int dateDisplay=0x7f050000;
-        public static final int pickDate=0x7f050001;
+        public static final int android_button=0x7f050000;
+        public static final int checkbox=0x7f050002;
+        public static final int edittext=0x7f050001;
+        public static final int radio_blue=0x7f050004;
+        public static final int radio_red=0x7f050003;
+        public static final int togglebutton=0x7f050005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/no/petter/TheActivity.java
+++ src/no/petter/TheActivity.java
@@ -2,16 +2,81 @@ package no.petter;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class TheActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        TextView tv = new TextView(this);
-//        tv.setText("Here's some text");
-//        setContentView(tv);
         setContentView(R.layout.main);
+        
+        ImageButton iButton = (ImageButton) findViewById(R.id.android_button);
+        iButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Log.i("tag", "button pushed");
+				// Perform action on clicks
+		        Toast.makeText(TheActivity.this, "Beep Bop", Toast.LENGTH_LONG).show();
+			}
+		});
+        
+        final EditText edittext = (EditText) findViewById(R.id.edittext);
+        edittext.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                  // Perform action on key press
+                  Toast.makeText(TheActivity.this, edittext.getText(), Toast.LENGTH_SHORT).show();
+                  return true;
+                }
+                return false;
+            }
+        });
+        
+        final CheckBox checkbox = (CheckBox) findViewById(R.id.checkbox);
+        checkbox.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on clicks
+                if (checkbox.isChecked()) {
+                    Toast.makeText(TheActivity.this, "Selected", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(TheActivity.this, "Not selected", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        
+        final RadioButton radio_red = (RadioButton) findViewById(R.id.radio_red);
+        final RadioButton radio_blue = (RadioButton) findViewById(R.id.radio_blue);
+        radio_red.setOnClickListener(radio_listener);
+        radio_blue.setOnClickListener(radio_listener);
+        
+        final ToggleButton togglebutton = (ToggleButton) findViewById(R.id.togglebutton);
+        togglebutton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on clicks
+                if (togglebutton.isChecked()) {
+                    Toast.makeText(TheActivity.this, "ON", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(TheActivity.this, "OFF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+    
+    OnClickListener radio_listener = new OnClickListener() {
+    	public void onClick(View v) {
+    		// Perform action on clicks
+    		RadioButton rb = (RadioButton) v;
+    		Toast.makeText(TheActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+    	}
+    };
 }
