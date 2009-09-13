fed2d72226dd05e26172f68c96f26a791ad07984
Petter Berntsen <petterb@gmail.com>
2009-09-13 18:45:44 +0200

    back in master?

--- gen/no/petter/R.java
+++ gen/no/petter/R.java
@@ -8,21 +8,24 @@
 package no.petter;
 
 public final class R {
+    public static final class array {
+        public static final int planets=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int dateDisplay=0x7f050000;
-        public static final int pickDate=0x7f050001;
+        public static final int dateDisplay=0x7f060000;
+        public static final int pickDate=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int typehere=0x7f040002;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int typehere=0x7f050002;
     }
 }
