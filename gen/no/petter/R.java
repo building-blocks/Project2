ee402ca5167ee591b3461a8ed23c9e5e9136791a
Petter Berntsen <petterb@gmail.com>
2009-09-13 18:42:56 +0200

    created a spinner

--- gen/no/petter/R.java
+++ gen/no/petter/R.java
@@ -8,6 +8,9 @@
 package no.petter;
 
 public final class R {
+    public static final class array {
+        public static final int planets=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,19 +18,14 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int android_button=0x7f050000;
-        public static final int checkbox=0x7f050002;
-        public static final int edittext=0x7f050001;
-        public static final int radio_blue=0x7f050004;
-        public static final int radio_red=0x7f050003;
-        public static final int togglebutton=0x7f050005;
+        public static final int spinner=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int typehere=0x7f040002;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int planet_prompt=0x7f050002;
     }
 }
--- src/no/petter/TheActivity.java
+++ src/no/petter/TheActivity.java
@@ -2,16 +2,8 @@ package no.petter;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RadioButton;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TheActivity extends Activity {
     /** Called when the activity is first created. */
@@ -20,63 +12,10 @@ public class TheActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        ImageButton iButton = (ImageButton) findViewById(R.id.android_button);
-        iButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Log.i("tag", "button pushed");
-				// Perform action on clicks
-		        Toast.makeText(TheActivity.this, "Beep Bop", Toast.LENGTH_LONG).show();
-			}
-		});
-        
-        final EditText edittext = (EditText) findViewById(R.id.edittext);
-        edittext.setOnKeyListener(new View.OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                  // Perform action on key press
-                  Toast.makeText(TheActivity.this, edittext.getText(), Toast.LENGTH_SHORT).show();
-                  return true;
-                }
-                return false;
-            }
-        });
-        
-        final CheckBox checkbox = (CheckBox) findViewById(R.id.checkbox);
-        checkbox.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-                if (checkbox.isChecked()) {
-                    Toast.makeText(TheActivity.this, "Selected", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(TheActivity.this, "Not selected", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        
-        final RadioButton radio_red = (RadioButton) findViewById(R.id.radio_red);
-        final RadioButton radio_blue = (RadioButton) findViewById(R.id.radio_blue);
-        radio_red.setOnClickListener(radio_listener);
-        radio_blue.setOnClickListener(radio_listener);
-        
-        final ToggleButton togglebutton = (ToggleButton) findViewById(R.id.togglebutton);
-        togglebutton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-                if (togglebutton.isChecked()) {
-                    Toast.makeText(TheActivity.this, "ONNN", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(TheActivity.this, "OFF", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
+        Spinner s = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this, R.array.planets, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
     }
-    
-    OnClickListener radio_listener = new OnClickListener() {
-    	public void onClick(View v) {
-    		// Perform action on clicks
-    		RadioButton rb = (RadioButton) v;
-    		Toast.makeText(TheActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
-    	}
-    };
 }
